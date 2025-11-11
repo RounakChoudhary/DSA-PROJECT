0da303242166aec9a9e61cac0bf8e8d51d17716a
Akkiiitj <b24cs1029@iitj.ac.in>
2025-11-11 21:19:05 +0530

    Initialized the graph structure

--- Network.h
+++ Network.h
@@ -18,3 +18,24 @@ struct Node {
     
     Node(float px = 0, float py = 0) : x(px), y(py), load(0), isActive(true) {}
 };
+
+class Network {
+private:
+    std::vector<Node> nodes;
+    std::vector<std::vector<Edge>> adjList;
+    int numNodes;
+    
+public:
+    Network(int n) : numNodes(n) {
+        nodes.resize(n);
+        adjList.resize(n);
+    }
+    
+    void setNodePosition(int id, float x, float y) {
+        if (id >= 0 && id < numNodes) {
+            nodes[id].x = x;
+            nodes[id].y = y;
+        }
+    }
+};
+#endif
